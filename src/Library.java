999cea4b6a9da9acf82d2fc11e7b169b11bc2453
Paulina Dudek <paulina.dudek@finsafe.io>
2018-03-11 14:47:11 +0100

    Library v0.2

--- src/Library.java
+++ src/Library.java
@@ -1,21 +1,22 @@
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.1";
+        final String appName = "Biblioteka v0.2";
 
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int releaseDate = 2010;
-        int pages = 296;
-        String publisher = "Greg";
-        String isbn = "9788373271890";
+        Book book1 = new Book();
+        book1.title="W Pustyni i w puszczy";
+        book1.author="Henryk Sienkiewicz";
+        book1.releaseDate = 2016;
+        book1.pages =234;
+        book1.publisher ="Greg";
+        book1.isbn="245342535235";
 
         System.out.print(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title +" "+
+                "\n"+ book1.author+
+                "\n"+ book1.releaseDate+
+                "\n"+ book1.pages+
+                "\n"+ book1.publisher+
+                "\n"+ book1.isbn);
     }
 }
