95c95cb8771a9e267599f37099006d38377730f7
Paulina Dudek <paulina.dudek@finsafe.io>
2018-03-12 14:49:52 +0100

    Library v0.7 - Scanner class added

--- src/app/Library.java
+++ src/app/Library.java
@@ -1,27 +1,22 @@
 package app;
 
 import data.Book;
+import utils.DataReader;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.6";
+        final String appName = "Biblioteka v0.7";
 
         Book []books = new Book[1000];
-        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg",
-                "9788373271890");
-        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", " Joshua Bloch", 2009,
-                352, "Helion", "9788324620845");
-        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
-                "Bert Bates, Katherine Sierra", 2008, 851, "McGraw-Hill Osborne Media",
-                "9780071591065");
+        DataReader dataReader = new DataReader();
 
-
-        System.out.print(appName);
+        System.out.println(appName);
+        System.out.println("Wprowadź nową książkę:");
+        books[0]=dataReader.readAndCreateBook();
+        dataReader.close();
 
         System.out.println("Książki w bibliotece:");
         books[0].printInfo();
-        books[1].printInfo();
-        books[2].printInfo();
         System.out.println("System może przechowywać "+books.length+" książek.");
     }
 }
---
+++ src/utils/DataReader.java
@@ -0,0 +1,36 @@
+package utils;//Created by Paulina on 2018-03-12.
+
+import data.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private Scanner input;
+
+    public DataReader(){
+        input = new Scanner(System.in);
+    }
+    public void close(){
+        input.close();
+    }
+
+    public Book readAndCreateBook(){
+        System.out.println("Tytuł: ");
+        String title = input.nextLine();
+        System.out.println("Autor: ");
+        String author = input.nextLine();
+        System.out.println("Wydawnictwo: ");
+        String publisher = input.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = input.nextLine();
+        System.out.println("Rok wydania: ");
+        int releaseDate = input.nextInt();
+        input.nextLine();
+        System.out.println("Ilość stron: ");
+        int pages = input.nextInt();
+        input.nextLine();
+
+        return new Book(title,author,releaseDate, pages,publisher,isbn);
+    }
+
+}
