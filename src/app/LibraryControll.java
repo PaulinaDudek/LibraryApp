a448de00cd98b62799a3d5f445b6b56f4d8d55d1
PaulinaDudek <dudekpaulinaa@gmail.com>
2018-06-05 23:31:34 +0200

    Adding inner class with options

--- src/app/LibraryControll.java
+++ src/app/LibraryControll.java
@@ -71,4 +71,35 @@ public class LibraryControll {
     private void printMagazines(){
         LibraryUtils.printMagazines(library);
     }
+
+    private enum Option{
+        EXIT (0,"Wyjście z programu"),
+        ADD_BOOK (1, "Dodanie książki"),
+        ADD_MAGAZINE (2, "Dodanie magazynu"),
+        PRINT_BOOKS (3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES (4, "Wyświetlenie dostępnych magazynów");
+
+        private int value;
+        private String description;
+
+        Option (int value, String description){
+            this.value=value;
+            this.description=description;
+        }
+
+        @Override
+        public String toString() {
+            return value+" "+description;
+        }
+
+        public static Option createFromInt(int option) throws NoSuchElementException{
+            Option result = null;
+            try{
+                result = Option.values()[option];
+            }catch (ArrayIndexOutOfBoundsException e){
+                throw new NoSuchElementException("Brak elementu o wskazanym ID");
+            }
+            return result;
+        }
+    }
 }
